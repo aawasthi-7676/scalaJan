ae3766ee11b2f92095a9fb41004bcf6f25a0d38d
Abhishek Awasthi <abhishekhcumca@gmail.com>
2022-01-14 14:48:19 +0530

    changes for deptDF for scalaPractise

--- scalaPractise.scala
+++ scalaPractise.scala
@@ -18,17 +18,19 @@ object scalaPractise {
     val deptDF = deptFile.toDF("ddeptno","depname")
 	
 	empDF.show(false)
-
+	deptDF.show(false)
+	
     empDF.createOrReplaceTempView("empview")
     deptDF.createOrReplaceTempView("deptview")
 
     val FinalDF = spark.sql("select ename,grade,sal,sex,edeptno,depname from empview e, deptview d where d.ddeptno = e.edeptno")
 
+	FinalDF.show(false)
     FinalDF.coalesce(1).write.format("csv")
       .option("header","true") //it will give header info in o/p file
       .mode("overwrite") //can be append, overwrite
       .save("C:/data/scalaPractise.csv")
 
-    FinalDF.show(false)
+   
   }
 }
